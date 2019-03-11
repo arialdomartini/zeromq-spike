6bcf71f35a6a45e425c9c85e73a25296743982f6
Arialdo Martini <arialdo.martini@tcpos.com>
2019-03-11 11:04:38 +0100

    A server responding to 5 clients sending 10.000 messages

---
+++ ZeroMQSpike/Spike.cs
@@ -0,0 +1,92 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using ZeroMQ;
+
+namespace ZeroMQSpike
+{
+    public class Spike
+    {
+        [Fact]
+        public void should_receive_messages()
+        {
+            const int numberOfMessages = 10_000;
+            const int numberOfClients = 5;
+
+            const string port = "5555";
+            var endpoint = $"tcp://*:{port}";
+
+            var server = new Server(endpoint);
+
+            Task.Run(() => server.Listen(numberOfMessages * numberOfClients));
+
+            Enumerable.Range(0, numberOfClients).ToList().AsParallel().ForAll(i =>
+            {
+                var client = new Client($"tcp://127.0.0.1:{port}");
+                client.Send(numberOfMessages);
+            });
+
+
+            server.Received.Should().Be(numberOfMessages * numberOfClients);
+        }
+    }
+
+    internal class Server
+    {
+        private readonly string _endpoint;
+
+        internal Server(string endpoint)
+        {
+            _endpoint = endpoint;
+        }
+
+        internal int Received { get; private set; }
+
+        internal void Listen(int numberOfMessages)
+        {
+            using (var context = new ZContext())
+            using (var responder = new ZSocket(context, ZSocketType.REP))
+            {
+                responder.Bind(_endpoint);
+                for (var i = 0; i < numberOfMessages; i++)
+                {
+                    using (responder.ReceiveFrame())
+                    {
+                        Received++;
+                        responder.Send(new ZFrame("received"));
+                    }
+                }
+            }
+        }
+    }
+
+    internal class Client
+    {
+        private readonly string _endpoint;
+
+        internal Client(string endpoint)
+        {
+            _endpoint = endpoint;
+        }
+
+        private int Sent { get; set; }
+
+        internal void Send(int numberOfMessages)
+        {
+            using (var context = new ZContext())
+            using (var requester = new ZSocket(context, ZSocketType.REQ))
+            {
+                requester.Connect(_endpoint);
+                for (var i = 0; i < numberOfMessages; i++)
+                {
+                    requester.Send(new ZFrame("message"));
+                    using(var frame = requester.ReceiveFrame())
+                    {
+                        Sent++;
+                    }
+                }
+            }
+        }
+    }
+}
